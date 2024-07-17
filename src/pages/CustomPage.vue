ce85140f2d7b9ff618531f90ab8f6754863b88c3
DeadFrost <harshil@deadfrost.dev>
2024-07-17 15:41:30 -0500

    Formatted cpage.vue

--- src/pages/CustomPage.vue
+++ src/pages/CustomPage.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
-import { computed } from "vue";
 import SpotifyToApple from "../components/SpotifyToApple.vue";
-
-const cfg = computed(() => {
-    return window.CiderApp.config.getRef();
-});
 </script>
+
 <template>
     <div class="q-pa-lg plugin-base">
         <h1 class="apple-heading">Spotify playlist converter</h1>
