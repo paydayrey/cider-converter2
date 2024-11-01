826f5915d50cf3bca9fdef36f99a5b55dac2899e
paydayrey <reyada@g.clemson.edu>
2024-11-01 11:09:56 -0400

    Update plugin.config.ts

--- src/plugin.config.ts
+++ src/plugin.config.ts
@@ -6,7 +6,7 @@ export default {
     name: 'Spotify to Cider 2 Converter',
     description: 'A fixed version of the plugin to convert Spotify playlists to Apple Music',
     version: '1.0',
-    author: 'Payday',
+    author: 'Payday', 'DeadFrost', 'Yaz Ninja' for their work on the original
     repo: 'https://github.com/paydayrey/cider-converter2',
     requirements: {
         minApiVersion: '2.0.0'
